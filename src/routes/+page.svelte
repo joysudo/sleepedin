d239b5e327989de65089cd1e642db0eaf7fba3eb
kat <katwang7749@gmail.com>
2026-04-25 18:30:28 -0500

    add tailwindcss, global css file (app.css), landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
     import PixelCanvas from '$lib/components/PixelCanvas.svelte';
 </script>
 
+<a href="/landing">link to landing (will remove later)</a>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
---
+++ src/routes/landing/+page.svelte
@@ -0,0 +1,3 @@
+<div class="items-center justify-center flex flex-col gap-4 w-full h-full bg-blue-300">
+    <img src="sleepedinlogo.png" alt="SleepedIn logo"/>
+</div>

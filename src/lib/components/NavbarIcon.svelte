9d2cf0e27150ff5dc0e38c600a99f79b178f64f6
kat <katwang7749@gmail.com>
2026-04-26 00:22:05 -0500

    add navbar and friends

---
+++ src/lib/components/NavbarIcon.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { iconUrl, iconAlt, text, link } = $props();
+</script>
+
+<a href={link}>
+    <div
+        class="flex flex-col items-center duration-200 hover:scale-105 active:scale-95"
+    >
+        <img src={iconUrl} alt={iconAlt} class="w-12" />
+        <h2 class="font-tommy-bold text-2xl text-purple-dark">{text}</h2>
+    </div>
+</a>

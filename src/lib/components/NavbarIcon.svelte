48bfb2037504918e777a8945be70e274beb2c885
kat <katwang7749@gmail.com>
2026-04-26 00:18:17 -0500

    navbar n shi

--- src/lib/components/NavbarIcon.svelte
+++ src/lib/components/NavbarIcon.svelte
@@ -1,8 +1,10 @@
 <script>
-    let { iconUrl, iconAlt, text } = $props();
+    let { iconUrl, iconAlt, text, link } = $props();
 </script>
 
-<div class="flex flex-col items-center">
-    <img src={iconUrl} alt={iconAlt} class="w-12" />
-    <h2 class="font-tommy-bold text-2xl text-purple-dark">{text}</h2>
-</div>
+<a href={link}>
+    <div class="flex flex-col items-center duration-200 hover:scale-105 active:scale-95">
+        <img src={iconUrl} alt={iconAlt} class="w-12" />
+        <h2 class="font-tommy-bold text-2xl text-purple-dark">{text}</h2>
+    </div>
+</a>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Navbar from "$lib/components/Navbar.svelte";
+</script>
+
+<div>
+    <Navbar />
+    <!-- clouds n bunnies n shi -->
+    <div></div>
+    <!-- the half with the scrollable divs -->
+    <div></div>
+</div>
